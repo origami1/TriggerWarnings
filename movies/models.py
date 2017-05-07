dd6dd61ac24ed90d204cd8d51a73d2b6c9f78415
jaime <jaime@chaseandscout.com>
2017-05-07 01:14:34 -0500

    Created Movies Model

--- movies/models.py
+++ movies/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from TriggerWarnings import trigger_types
+
 # Create your models here.
+
+# All we need is a DB for the movies
+class Movies(models.Model):
+    title = models.CharField(max_length=100,
+                             help_text="Keep it under 100 characters, using a release date if needed",
+                             unique = True)
+    for trigger, descr in trigger_types.types:
+        locals()[trigger] = models.BooleanField(default=False, verbose_name=descr)
+
+    del locals()['trigger']
